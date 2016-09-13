2a0a64b000d247cd376f77abae1758cf4636181f
Daniel Bayley <daniel.bayley@me.com>
2016-09-13 19:45:00 +0100

    Add Popcorn Time

---
+++ Casks/popcorn-time.rb
@@ -0,0 +1,18 @@
+cask 'popcorn-time' do
+  version '0.3.9'
+  sha256 '390c29cc9f4187cf068a0d53897528fb576d9e4076fd1fa47354086cc2d3770b'
+
+  url "https://popcorntime.sh/download/build/Popcorn-Time-#{version}-Mac.tar.xz"
+  name 'Popcorn-Time'
+  homepage 'https://popcorntime.sh'
+  license :gpl
+
+  app 'Popcorn-Time.app'
+
+  uninstall rmdir: '~/Library/Application Support/Popcorn-Time'
+  zap delete: [
+        '~/Library/Application Support/Popcorn-Time',
+        '~/Library/Saved Application State/com.nw-builder.popcorn-time.savedState',
+        '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.nw-builder.popcorn-time.sfl'
+      ]
+end
