8f7a9f1d73839d5a0e3d96e275042615ec50c293
Daniel Bayley <daniel.bayley@me.com>
2016-09-15 17:56:07 +0100

    Add symlinks

---
+++ Formula/symlinks.rb
@@ -0,0 +1,15 @@
+class Symlinks < Formula
+  desc "is a simple tool to help find and remedy problematic symbolic links."
+  homepage "https://github.com/brandt/symlinks"
+  url "https://github.com/brandt/symlinks/archive/v1.4.3.tar.gz"
+  sha256 "27105b2898f28fd53d52cb6fa77da1c1f3b38e6a0fc2a66bf8a25cd546cb30b2"
+
+  def install
+    system "make", "install"
+    bin.install "symlinks"
+  end
+
+  test do
+    system "#{bin}/symlinks"
+  end
+end
