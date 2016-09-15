e6021d9d6a78639d5b40516c9543c115e6e4bf48
Daniel Bayley <daniel.bayley@me.com>
2016-09-15 17:55:56 +0100

    Add aliasPath

---
+++ Formula/aliaspath.rb
@@ -0,0 +1,18 @@
+class Aliaspath < Formula
+  desc "prints the POSIX path to the original of a Finder alias."
+  homepage "https://github.com/rptb1/aliasPath"
+  url "https://github.com/rptb1/aliasPath/archive/master.zip"
+  version "1.0"
+  sha256 "45f586186bc67a8c8dfface8044436354673277a873b347861cb1cca0024ac2b"
+
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "-project", "aliasPath.xcodeproj"
+    bin.install "build/Release/aliasPath"
+  end
+
+  test do
+    system "#{bin}/aliaspath"
+  end
+end
