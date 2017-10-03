145480ba53ac707c2b6299029dfe3a0fd3278670
Daniel Bayley <daniel.bayley@me.com>
2017-10-04 00:30:15 +0100

    Add Adobe Extension Manager CLI

---
+++ Formula/adobe-exmancmd.rb
@@ -0,0 +1,16 @@
+class AdobeExmancmd < Formula
+  desc "Install add-ons and extensions for Adobe apps."
+  homepage "https://helpx.adobe.com/creative-cloud/kb/installingextensionsandaddons.html"
+  url "https://www.adobeexchange.com/ExManCmd_mac.zip"
+  version "7.3.3.481"
+  sha256 "9ed5b6dc8c48a1f76fbab87f53aa87e85b5b718f44fa6ce46be12cc3f5880475"
+
+  def install
+    lib.install Dir["Contents/*"]
+    bin.install_symlink lib/"MacOS/ExManCmd"
+  end
+
+  test do
+    system bin/"ExManCmd", "--help"
+  end
+end
