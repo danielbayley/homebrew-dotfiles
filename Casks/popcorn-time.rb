6739a3507ba85fbf85ac585d7371d62d2a7a7c30
Daniel <daniel.bayley@me.com>
2019-11-20 19:14:09 +0000

    Fix Popcorn Time cask

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -1,17 +1,22 @@
 cask 'popcorn-time' do
   version '0.3.10'
-  sha256 '7d2cd37837963df8bb3c3c8601e44142a8314ac71e943f544f9a638da1c1330f'
+  sha256 '62002814ddac7587e447d0f8e7db5d7359ccca77893b79223f7b1f5c7d8992bf'
 
-  url "https://popcorntime.sh/download/build/Popcorn-Time-#{version}-Mac.zip"
-  name 'Popcorn-Time'
-  homepage 'https://popcorntime.sh'
+  url "https://mirror03.popcorntime.sh/repo/build/Popcorn-Time-#{version}-Mac.zip"
+  name 'Popcorn Time'
+  homepage 'https://popcorntime.sh/'
 
-  app 'Popcorn-Time.app', target: 'Popcorn Time.app'
+  app 'Popcorn-Time.app'
 
-  uninstall rmdir: '~/Library/Application Support/Popcorn-Time'
-  zap delete: [
-        '~/Library/Application Support/Popcorn-Time',
-        '~/Library/Saved Application State/com.nw-builder.popcorn-time.savedState',
-        '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.nw-builder.popcorn-time.sfl'
-      ]
+  bundle_id = 'com.nw-builder.popcorn-time'
+  uninstall quit: bundle_id
+
+  zap trash: [
+               "~/Library/Preferences/#{bundle_id}.plist",
+               '~/Library/Application Support/Popcorn-Time',
+               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
+               '~/Library/Application Support/configstore/popcorn-time.json',
+               "~/Library/Saved Application State/#{bundle_id}.savedState",
+               '~/Library/Caches/Popcorn-Time',
+             ]
 end
