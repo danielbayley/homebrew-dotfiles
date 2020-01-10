e28b82ad9abc5c787b0e546aedf9a96928c810c4
Daniel Bayley <daniel.bayley@me.com>
2020-01-10 02:14:24 +0000

    Fix Icon Tester

--- Casks/icon-tester.rb
+++ Casks/icon-tester.rb
@@ -2,12 +2,12 @@ cask 'icon-tester' do
   version '1.0'
   sha256 '1c7fe01951d79df18d85b2b0217c285d0161f6a91a18c91b375bf62d632a58d8'
 
-  # kennyadr.com was verified as official when first introduced to the cask
-  url 'http://kennyadr.com/archives/icontester.zip'
-  name 'Icon Tester' # https://github.com/aroben/IconTester
-  homepage 'https://dribbble.com/shots/47777-Icon-Tester'
+  url 'https://download.informer.com/mac-1192982262-b0fe4f19-698ebf6f/icontester.zip'
+  appcast 'https://macdownload.informer.com/icon-tester/download'
+  name 'Icon Tester'
+  homepage "https://macdownload.informer.com/icon-tester/#{version}"
 
-  app "#{name[0]}/#{name[0]}.app"
+  app "#{name.first}/#{name.first}.app"
 
-  zap delete: ['~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adriankenny.icon-tester.sfl']
+  zap delete: '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adriankenny.icon-tester.sfl*'
 end
